020f31b82a266028d4ecfe4e842644cb1ccc8014
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2021-03-04 09:43:12 +0530

    Update model
    - correct field type of roll_no
    - correct max_digits allowed for percentage
    - Leave default=1 for now

--- marksmanager/leaderboard/models.py
+++ marksmanager/leaderboard/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator
 
 
 class Leaderboard(models.Model):
-    roll_no = models.EmailField(max_length=100, unique=True)
+    roll_no = models.CharField(max_length=100, unique=True)
     name = models.CharField(max_length=100)
     maths_marks = models.PositiveIntegerField(
         default=1, validators=[MaxValueValidator(100)]
@@ -18,6 +18,6 @@ class Leaderboard(models.Model):
         default=1, validators=[MaxValueValidator(300)]
     )
     percentage = models.DecimalField(
-        max_digits=3, decimal_places=2, default=1, validators=[MaxValueValidator(100)]
+        max_digits=5, decimal_places=2, default=1, validators=[MaxValueValidator(100)]
     )
     created_at = models.DateTimeField(auto_now_add=True)
