53a15cbc4c2c37eb10869ae28fc71d7bc56e8c81
HeroicHitesh <hiteshkumar_1mv17cs042@sirmvit.edu>
2021-03-04 07:53:02 +0530

    Create Leaderboard model

--- marksmanager/leaderboard/models.py
+++ marksmanager/leaderboard/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+
+class Leaderboard(models.Model):
+    roll_no = models.EmailField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    maths_marks = models.PositiveIntegerField(
+        default=1, validators=[MaxValueValidator(100)]
+    )
+    physics_marks = models.PositiveIntegerField(
+        default=1, validators=[MaxValueValidator(100)]
+    )
+    chemistry_marks = models.PositiveIntegerField(
+        default=1, validators=[MaxValueValidator(100)]
+    )
+    total_marks = models.PositiveIntegerField(
+        default=1, validators=[MaxValueValidator(300)]
+    )
+    percentage = models.DecimalField(
+        max_digits=3, decimal_places=2, default=1, validators=[MaxValueValidator(100)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
